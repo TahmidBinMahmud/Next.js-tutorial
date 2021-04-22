f5f3dd93f05dd156eda22809b0b46dc2fcc8c898
TahmidBinMahmud <1510927@iub.edu.bd>
2021-04-23 04:56:56 +0600

    createNote added

--- notes-app/pages/_app.js
+++ notes-app/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css';
 import Layout from '../components/Layout';
+import '../css/style.css'
 
 function MyApp({ Component, pageProps }) {
   return <Layout><Component {...pageProps} /></Layout>
